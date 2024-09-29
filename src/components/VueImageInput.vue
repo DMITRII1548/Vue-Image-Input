7407469b53bf960979afdae0929167c75108ca2e
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2024-09-29 13:23:34 +0600

    Componenent rewrited to composition API from options API

--- src/components/VueImageInput.vue
+++ src/components/VueImageInput.vue
@@ -4,104 +4,160 @@
     :style="`border-color: ${borderColor}; padding: ${padding ?? (paddingY ?? '0') + ' ' + (paddingX ?? '0')}; padding-left: ${paddingLeft}; padding-right: ${paddingRight}; padding-top: ${paddingTop}; padding-bottom: ${paddingBottom}`"
     @drop="handleDrop" 
     @dragover.prevent>
+    
     <template v-if="!imageSrc">
       <input 
         class="input" 
         type="file" 
-        v-on:change="onChange" 
+        @change="onChange" 
         :accept="mimes"
-        @input="$emit('update:file', $event.target.files[0])"
-        multiple
         ref="input">
       <button 
         @click.prevent="openInput" 
         :class="`${size} btn`"
-        :style="`background: ${background}; color: ${textColor}; font-size: ${fontSize}; border-radius: ${bgRounded}`">{{ title }}</button>
+        :style="`background: ${background}; color: ${textColor}; font-size: ${fontSize}; border-radius: ${bgRounded}`">
+        {{ title }}
+      </button>
     </template>
+    
     <template v-if="imageSrc">
       <img :src="imageSrc" class="w-full image" :style="`border-radius: ${imageRounded}`">
       <div 
         @click="deleteFile" 
         class="close"
         ref="close">
-          <div class="close-before" :style="`background: ${closeBtnColor}`" ref="closeBefore"></div>
-          <div class="close-after" :style="`background: ${closeBtnColor}`" ref="closeAfter"></div>
-        </div>
+        <div class="close-before" :style="`background: ${closeBtnColor}`" ref="closeBefore"></div>
+        <div class="close-after" :style="`background: ${closeBtnColor}`" ref="closeAfter"></div>
+      </div>
     </template>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'VueImageInput',
+<script setup>
+import { ref, computed } from 'vue'
 
-  props: {
-    title: String, // Drop here
-    fontSize: String,
-    size: String,
-    background: String,
-    textColor: String,
-    border: String,
-    borderColor: String,
-    mimes: String, //'.jpg,.png',
-    imageRounded: String,
-    closeBtnColor: String,
-    closeBtn: String,
-    bgRounded: String,
-    padding: String, //default 5px
-    paddingLeft: String,
-    paddingRight: String,
-    paddingTop: String,
-    paddingBottom: String,
-    paddingX: String,
-    paddingY: String
+const emit = defineEmits()
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Drop here'
+  }, 
+  fontSize: {
+    type: String,
+    default: ''
   },
-
-  data() {
-    return {
-      imageSrc: '',
-      file: [],
-    }
+  size: {
+    type: String,
+    default: ''
   },
-
-  methods: {
-    onChange(event) {
-      this.file = event.target.files[0]
-      this.createImage()
-    },
-
-    openInput() {
-      this.$refs.input.click()
-    },
-
-    createImage() {
-      const reader = new FileReader()
-
-      reader.onload = (e) => {
-        this.imageSrc = e.target.result
-      };
-
-      reader.readAsDataURL(this.file)
-    },
-
-    deleteFile() {
-      this.$emit('update:file', [])
-      this.imageSrc = ''
-      this.file = []
-    },
-
-    handleDrop(event) {
-      event.preventDefault()
-      this.file = event.dataTransfer.files[0]
-      this.createImage()
-    }
+  background: {
+    type: String,
+    default: ''
   },
-
-  computed: {
-    containerSize() {
-      return 'w' + this.size.slice(4)
-    },
+  textColor: {
+    type: String,
+    default: ''
   },
+  border: {
+    type: String,
+    default: ''
+  },
+  borderColor: {
+    type: String,
+    default: ''
+  },
+  mimes: {
+    type: String,
+    default: '.jpg,.png'
+  },
+  imageRounded: {
+    type: String,
+    default: ''
+  },
+  closeBtnColor: {
+    type: String,
+    default: ''
+  },
+  closeBtn: {
+    type: String,
+    default: ''
+  },
+  bgRounded: {
+    type: String,
+    default: ''
+  },
+  padding: {
+    type: String,
+    default: '5px'
+  },
+  paddingLeft: {
+    type: String,
+    default: ''
+  },
+  paddingRight: {
+    type: String,
+    default: ''
+  },
+  paddingTop: {
+    type: String,
+    default: ''
+  },
+  paddingBottom: {
+    type: String,
+    default: ''
+  },
+  paddingX: {
+    type: String,
+    default: ''
+  },
+  paddingY: {
+    type: String,
+    default: ''
+  }
+})
+
+const imageSrc = ref('')
+const file = ref(null)
+const input = ref(null) 
+
+const containerSize = computed(() => {
+  return 'w' + props.size.slice(4)
+})
+
+
+// Methods
+const onChange = (event) => {
+  file.value = event.target.files[0]
+  emit('update:file', [file.value])
+  createImage()
+}
+
+const openInput = () => {
+  input.value.click()
+}
+
+const createImage = () => {
+  const reader = new FileReader()
+  reader.onload = (e) => {
+    imageSrc.value = e.target.result
+  }
+  if (file.value) {
+    reader.readAsDataURL(file.value)
+  }
+}
+
+const deleteFile = () => {
+  emit('update:file', [])
+  imageSrc.value = ''
+  file.value = null
+}
+
+const handleDrop = (event) => {
+  event.preventDefault()
+  file.value = event.dataTransfer.files[0]
+  createImage()
+  emit('update:file', [file.value]) 
 }
 </script>
 
