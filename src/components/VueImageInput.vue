d940c03dfb8725fa4147effeee6057695cb7739e
Dmitrii <96414168+DMITRII1548@users.noreply.github.com>
2025-04-21 22:12:16 +0600

    Add imageSrc attribute

--- src/components/VueImageInput.vue
+++ src/components/VueImageInput.vue
@@ -3,30 +3,33 @@
     :class="`${containerSize} container ${border}`" 
     :style="`border-color: ${borderColor}; padding: ${padding ?? (paddingY ?? '0') + ' ' + (paddingX ?? '0')}; padding-left: ${paddingLeft}; padding-right: ${paddingRight}; padding-top: ${paddingTop}; padding-bottom: ${paddingBottom}`"
     @drop="handleDrop" 
-    @dragover.prevent>
-    
-    <template v-if="!imageSrc">
+    @dragover.prevent
+  >
+    <template v-if="!localImageSrc">
       <input 
         class="input" 
         type="file" 
         @change="onChange" 
         :accept="mimes"
         :required="required"
-        ref="input">
+        ref="input"
+      />
       <button 
         @click.prevent="openInput" 
         :class="`${size} btn`"
-        :style="`background: ${background}; color: ${textColor}; font-size: ${fontSize}; border-radius: ${bgRounded}`">
+        :style="`background: ${background}; color: ${textColor}; font-size: ${fontSize}; border-radius: ${bgRounded}`"
+      >
         {{ title }}
       </button>
     </template>
-    
-    <template v-if="imageSrc">
-      <img :src="imageSrc" class="w-full image" :style="`border-radius: ${imageRounded}`">
+
+    <template v-else>
+      <img :src="localImageSrc" class="w-full image" :style="`border-radius: ${imageRounded}`" />
       <div 
         @click="deleteFile" 
         class="close"
-        ref="close">
+        ref="close"
+      >
         <div class="close-before" :style="`background: ${closeBtnColor}`" ref="closeBefore"></div>
         <div class="close-after" :style="`background: ${closeBtnColor}`" ref="closeAfter"></div>
       </div>
@@ -35,103 +38,111 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 
 const emit = defineEmits()
 
 const props = defineProps({
-  title: {
-    type: String,
-    default: 'Drop here'
+  title: { 
+    type: String, 
+    default: 'Drop here' 
   }, 
-  required: {
-    type: Boolean,
-    default: false,
+  required: { 
+    type: Boolean, 
+    default: false 
   },
-  fontSize: {
-    type: String,
-    default: ''
+  fontSize: { 
+    type: String, 
+    default: '' 
   },
-  size: {
-    type: String,
-    default: ''
+  size: { 
+    type: String, 
+    default: '' 
   },
-  background: {
-    type: String,
-    default: ''
+  background: { 
+    type: String, 
+    default: '' 
   },
-  textColor: {
-    type: String,
-    default: ''
+  textColor: { 
+    type: String, 
+    default: '' 
   },
-  border: {
-    type: String,
-    default: ''
+  border: { 
+    type: String, 
+    default: '' 
   },
-  borderColor: {
-    type: String,
-    default: ''
+  borderColor: { 
+    type: String, 
+    default: '' 
   },
-  mimes: {
-    type: String,
-    default: '.jpg,.png'
+  mimes: { 
+    type: String, 
+    default: '.jpg,.png' 
   },
-  imageRounded: {
-    type: String,
-    default: ''
+  imageRounded: { 
+    type: String, 
+    default: '' 
   },
-  closeBtnColor: {
-    type: String,
-    default: ''
+  closeBtnColor: { 
+    type: String, 
+    default: '' 
   },
-  closeBtn: {
-    type: String,
-    default: ''
+  closeBtn: { 
+    type: String, 
+    default: '' 
   },
-  bgRounded: {
-    type: String,
-    default: ''
+  bgRounded: { 
+    type: String, 
+    default: '' 
   },
-  padding: {
-    type: String,
-    default: ''
+  padding: { 
+    type: String, 
+    default: '' 
   },
-  paddingLeft: {
-    type: String,
-    default: ''
+  paddingLeft: { 
+    type: String, 
+    default: '' 
   },
-  paddingRight: {
-    type: String,
-    default: ''
+  paddingRight: { 
+    type: String, 
+    default: '' 
   },
-  paddingTop: {
-    type: String,
-    default: ''
+  paddingTop: { 
+    type: String, 
+    default: '' 
   },
-  paddingBottom: {
-    type: String,
-    default: ''
+  paddingBottom: { 
+    type: String, 
+    default: '' 
   },
-  paddingX: {
-    type: String,
-    default: ''
+  paddingX: { 
+    type: String, 
+    default: '' 
   },
-  paddingY: {
-    type: String,
-    default: ''
+  paddingY: { 
+    type: String, 
+    default: '' 
+  },
+  imageSrc: { 
+    type: String, 
+    default: '' 
   }
 })
 
-const imageSrc = ref('')
 const file = ref(null)
-const input = ref(null) 
+const input = ref(null)
+const localImageSrc = ref(props.imageSrc)
+
+watch(() => props.imageSrc, (val) => {
+  localImageSrc.value = val
+})
 
 const containerSize = computed(() => {
   return 'w' + props.size.slice(4)
 })
 
+// methods
 
-// Methods
 const onChange = (event) => {
   file.value = event.target.files[0]
   emit('update:file', [file.value])
@@ -145,7 +156,8 @@ const openInput = () => {
 const createImage = () => {
   const reader = new FileReader()
   reader.onload = (e) => {
-    imageSrc.value = e.target.result
+    localImageSrc.value = e.target.result
+    emit('update:imageSrc', e.target.result)
   }
   if (file.value) {
     reader.readAsDataURL(file.value)
@@ -154,7 +166,8 @@ const createImage = () => {
 
 const deleteFile = () => {
   emit('update:file', [])
-  imageSrc.value = ''
+  emit('update:imageSrc', '')
+  localImageSrc.value = ''
   file.value = null
 }
 
